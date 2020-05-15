af9a18e49a3cd6aee21eb30fef69ea196d829da5
Svetlana <antesevicceca@gmail.com>
2020-05-16 00:52:27 +0200

    Added constructor in TableSimilarity class.

--- src/similarity/TableSimilarity.java
+++ src/similarity/TableSimilarity.java
@@ -3,7 +3,20 @@ package similarity;
 import ucm.gaia.jcolibri.exception.NoApplicableSimilarityFunctionException;
 import ucm.gaia.jcolibri.method.retrieve.NNretrieval.similarity.LocalSimilarityFunction;
 
+import java.util.List;
+
 public class TableSimilarity implements LocalSimilarityFunction {
+
+    private double matrix[][];
+    List<String> categories;
+
+    public TableSimilarity(List<String> categories) {
+        this.categories = categories;
+        int n = categories.size();
+        matrix = new double[n][n];
+        for (int i=0; i<n; i++)
+            matrix[i][i] = 1;
+    }
 
     @Override
     public double compute(Object o, Object o1) throws NoApplicableSimilarityFunctionException {
