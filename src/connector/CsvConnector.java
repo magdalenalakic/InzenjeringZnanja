dd3c4f86bf5a3a1f0ac85f9fa57ca01fc8f69f31
Svetlana <antesevicceca@gmail.com>
2020-05-17 21:20:41 +0200

    Edited csv connector.

--- src/connector/CsvConnector.java
+++ src/connector/CsvConnector.java
@@ -10,8 +10,10 @@ import ucm.gaia.jcolibri.util.FileIO;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 public class CsvConnector implements Connector {
 
@@ -42,7 +44,7 @@ public class CsvConnector implements Connector {
         Pacijent pacijent = new Pacijent();
 
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(FileIO.openFile("data/zadatak1.csv")));
+            BufferedReader br = new BufferedReader(new InputStreamReader(FileIO.openFile("csv-files/fizikalni-pregled.csv")));
             if (br == null)
                 throw new Exception("Error opening file");
 
@@ -83,7 +85,7 @@ public class CsvConnector implements Connector {
         }
 
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(FileIO.openFile("data/zadatak1.csv")));
+            BufferedReader br = new BufferedReader(new InputStreamReader(FileIO.openFile("csv-files/dodatna-ispitivanja.csv")));
             if (br == null)
                 throw new Exception("Error opening file");
 
@@ -96,33 +98,84 @@ public class CsvConnector implements Connector {
 
                 for(int i = 0; i < listaRez.length; i++){
                     String[] rez = listaRez[i].split("=");
-//                    if(rez[0].equals("analizaKrvi")){
-//
-//                    }else if(rez[0].equals("ekg")){
-//
-//                    }else if(rez[0].equals("ehokardiografija")){
-//
-//                    }else if(rez[0].equals("ergometrija")){
-//
-//                    }else if(rez[0].equals("koronarnaAngiografija")){
-//
-//                    }else if(rez[0].equals("rendgen")){
-//
-//                    }else if(rez[0].equals("holter24")){
-//
-//                    }else if(rez[0].equals("ct")){
-//
-//                    }
+
+                    String[] rezultati = rez[1].split("/+");
+                    List<String> listaRezultata = new ArrayList<>();
+                    for(int j = 0; j < rezultati.length; j++){
+                        listaRezultata.add(rezultati[j]);
+                    }
+
+
+                    if(rez[0].equals("analizaKrvi")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.analizaKrvi, listaRezultata);
+                    }else if(rez[0].equals("ekg")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.ekg, listaRezultata);
+                    }else if(rez[0].equals("ehokardiografija")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.ehokardiografija, listaRezultata);
+                    }else if(rez[0].equals("ergometrija")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.ergometrija, listaRezultata);
+                    }else if(rez[0].equals("koronarnaAngiografija")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.koronarnaAngiografija, listaRezultata);
+                    }else if(rez[0].equals("rendgen")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.rendgen, listaRezultata);
+                    }else if(rez[0].equals("holter24")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.holter24, listaRezultata);
+                    }else if(rez[0].equals("ct")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.ct, listaRezultata);
+                    }
                 }
-
-
 
             }
             br.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(FileIO.openFile("csv-files/dodatna-ispitivanja.csv")));
+            if (br == null)
+                throw new Exception("Error opening file");
 
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("#") || (line.length() == 0))
+                    continue;
+                String[] values = line.split(";");
+                String[] listaRez = values[1].split(",");
+
+                for(int i = 0; i < listaRez.length; i++){
+                    String[] rez = listaRez[i].split("=");
+
+                    String[] rezultati = rez[1].split("/+");
+                    List<String> listaRezultata = new ArrayList<>();
+                    for(int j = 0; j < rezultati.length; j++){
+                        listaRezultata.add(rezultati[j]);
+                    }
+
+
+                    if(rez[0].equals("analizaKrvi")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.analizaKrvi, listaRezultata);
+                    }else if(rez[0].equals("ekg")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.ekg, listaRezultata);
+                    }else if(rez[0].equals("ehokardiografija")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.ehokardiografija, listaRezultata);
+                    }else if(rez[0].equals("ergometrija")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.ergometrija, listaRezultata);
+                    }else if(rez[0].equals("koronarnaAngiografija")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.koronarnaAngiografija, listaRezultata);
+                    }else if(rez[0].equals("rendgen")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.rendgen, listaRezultata);
+                    }else if(rez[0].equals("holter24")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.holter24, listaRezultata);
+                    }else if(rez[0].equals("ct")){
+                        pacijent.getListaRezultataDodatnihIspitivanja().put(DodatnaIspitivanjaEnum.ct, listaRezultata);
+                    }
+                }
+
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 //                cbrCase.setDescription(movieDescription);
 //                cases.add(cbrCase);
