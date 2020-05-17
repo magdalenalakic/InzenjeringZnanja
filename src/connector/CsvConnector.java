3a24e7fac91945514d702859e5742ab5fb1e3527
Svetlana <antesevicceca@gmail.com>
2020-05-17 19:02:13 +0200

    Changed connector,table similarity and app file.

--- src/connector/CsvConnector.java
+++ src/connector/CsvConnector.java
@@ -4,9 +4,13 @@ import ucm.gaia.jcolibri.cbrcore.CBRCase;
 import ucm.gaia.jcolibri.cbrcore.CaseBaseFilter;
 import ucm.gaia.jcolibri.cbrcore.Connector;
 import ucm.gaia.jcolibri.exception.InitializingException;
+import ucm.gaia.jcolibri.util.FileIO;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Collection;
+import java.util.LinkedList;
 
 public class CsvConnector implements Connector {
 
@@ -32,7 +36,37 @@ public class CsvConnector implements Connector {
 
     @Override
     public Collection<CBRCase> retrieveAllCases() {
-        return null;
+        LinkedList<CBRCase> cases = new LinkedList<CBRCase>();
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(FileIO.openFile("data/zadatak1.csv")));
+            if (br == null)
+                throw new Exception("Error opening file");
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("#") || (line.length() == 0))
+                    continue;
+                String[] values = line.split(";");
+
+                CBRCase cbrCase = new CBRCase();
+
+//                MovieDescription movieDescription = new MovieDescription();
+//                movieDescription.setGender(values[1]);
+//                movieDescription.setAge(Integer.parseInt(values[2]));
+//                movieDescription.setScore(Integer.parseInt(values[4]));
+//                movieDescription.setTitle(values[6]);
+//                movieDescription.setYear(Integer.parseInt(values[7]));
+//                movieDescription.setGenre(values[8]);
+//
+//                cbrCase.setDescription(movieDescription);
+//                cases.add(cbrCase);
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cases;
     }
 
     @Override
--- src/similarity/TableSimilarity.java
+++ src/similarity/TableSimilarity.java
@@ -18,13 +18,34 @@ public class TableSimilarity implements LocalSimilarityFunction {
             matrix[i][i] = 1;
     }
 
+    public void setSimilarity(String value1, String value2, double sim) {
+        setSimilarity(value1, value2, sim, sim);
+    }
+
+    public void setSimilarity(String value1, String value2, double sim1, double sim2) {
+        int index1 = categories.indexOf(value1);
+        int index2 = categories.indexOf(value2);
+        if (index1 != -1 && index2 != -1) {
+            matrix[index1][index2] = sim1;
+            matrix[index2][index1] = sim2;
+        }
+    }
+
     @Override
-    public double compute(Object o, Object o1) throws NoApplicableSimilarityFunctionException {
+    public double compute(Object value1, Object value2) throws NoApplicableSimilarityFunctionException {
+        int index1 = categories.indexOf(value1);
+        int index2 = categories.indexOf(value2);
+        if (index1 != -1 && index2 != -1)
+            return matrix[index1][index2];
+        if (value1 != null && value1.equals(value2))
+            return 1;
         return 0;
     }
 
     @Override
-    public boolean isApplicable(Object o, Object o1) {
+    public boolean isApplicable(Object value1, Object value2) {
+        if (value1 instanceof String && value2 instanceof String)
+            return true;
         return false;
     }
 }
