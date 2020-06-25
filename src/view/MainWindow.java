6e7219ccbcfb378a42335954134ec57ee63aeb2c
Magdalena <magdalena.lakic@gmail.com>
2020-06-25 23:12:50 +0200

    Edited panel for detail view

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -1189,57 +1189,57 @@ public class MainWindow extends JFrame {
                 UnesiRezDIWindow.getInstance().setUnetiRezDI(new ArrayList<>());
             }
         }
-        JLabel imeP = new JLabel(trenutnoAktivanPacijent.getIme());
-        imeP.setFont(new Font("Tahoma", Font.BOLD, 18));
+        Label imeP = new Label("Ime pacijenta: "+ trenutnoAktivanPacijent.getIme() );
+        imeP.setFont(new Font("Tahoma", Font.BOLD, 14));
 
         boxCentar.add(imeP);
-        boxCentar.add(new JLabel("Godine: " + String.valueOf(trenutnoAktivanPacijent.getGodine())));
+        boxCentar.add(new Label("Godine: " + String.valueOf(trenutnoAktivanPacijent.getGodine())));
 
         if(trenutnoAktivanPacijent.getTezina().equals(TezinaEnum.normalnaTezina)){
-            boxCentar.add(new JLabel("Tezina: "+ "NORMALNA"));
+            boxCentar.add(new Label("Tezina: "+ "NORMALNA"));
         }else if(trenutnoAktivanPacijent.getTezina().equals(TezinaEnum.smanjenaTezina)){
-            boxCentar.add(new JLabel("Tezina: "+ "SMANJENA"));
+            boxCentar.add(new Label("Tezina: "+ "SMANJENA"));
         }else{
-            boxCentar.add(new JLabel("Tezina: "+ "POVECANA"));
+            boxCentar.add(new Label("Tezina: "+ "POVECANA"));
         }
 
         if(trenutnoAktivanPacijent.getPusac().equals(true)){
-            boxCentar.add(new JLabel("Pusac: " + "DA"));
+            boxCentar.add(new Label("Pusac: " + "DA"));
         }else{
-            boxCentar.add(new JLabel("Pusac: " + "NE"));
+            boxCentar.add(new Label("Pusac: " + "NE"));
         }
 
         if(trenutnoAktivanPacijent.getDijabeticar().equals(true)){
-            boxCentar.add(new JLabel("Dijabeticar: " + "DA"));
+            boxCentar.add(new Label("Dijabeticar: " + "DA"));
         }else{
-            boxCentar.add(new JLabel("Dijabeticar: " + "NE"));
+            boxCentar.add(new Label("Dijabeticar: " + "NE"));
         }
 
         if(trenutnoAktivanPacijent.getAsmaticar().equals(true)){
-            boxCentar.add(new JLabel("Asmaticar: " + "DA"));
+            boxCentar.add(new Label("Asmaticar: " + "DA"));
         }else{
-            boxCentar.add(new JLabel("Asmaticar: " + "NE"));
+            boxCentar.add(new Label("Asmaticar: " + "NE"));
         }
 
         if(trenutnoAktivanPacijent.getFizickaAktivnost().equals(true)){
-            boxCentar.add(new JLabel("Fizicka aktivnost: " + "DA"));
+            boxCentar.add(new Label("Fizicka aktivnost: " + "DA"));
         }else{
-            boxCentar.add(new JLabel("Fizicka aktivnost: " + "NE"));
+            boxCentar.add(new Label("Fizicka aktivnost: " + "NE"));
         }
 
         if(trenutnoAktivanPacijent.getPol() == PolEnum.Z){
             if(trenutnoAktivanPacijent.getTrudnoca().equals(true)){
-                boxCentar.add(new JLabel("Trudnoca: " + "DA"));
+                boxCentar.add(new Label("Trudnoca: " + "DA"));
             }else{
-                boxCentar.add(new JLabel("Trudnoca: " + "NE"));
+                boxCentar.add(new Label("Trudnoca: " + "NE"));
             }
 
         }
 
         if(trenutnoAktivanPacijent.getAlergican().equals(true)){
-            boxCentar.add(new JLabel("Alergija: " + "DA"));
+            boxCentar.add(new Label("Alergija: " + "DA"));
         }else{
-            boxCentar.add(new JLabel("Alergija: " + "NE"));
+            boxCentar.add(new Label("Alergija: " + "NE"));
         }
 
         JButton zapocni = new JButton("ZAPOCNI");
